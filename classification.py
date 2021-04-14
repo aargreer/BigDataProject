62d1276808ffe99cba433f0bc52703f28f69a530
aargreer <aagreer@lakeheadu.ca>
2021-04-14 17:25:40 -0400

    removed unnecessary classifiers

--- classification.py
+++ classification.py
@@ -10,11 +10,7 @@ from matplotlib.colors import ListedColormap
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.datasets import make_moons, make_circles, make_classification
-from sklearn.neural_network import MLPClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
-from sklearn.gaussian_process import GaussianProcessClassifier
-from sklearn.gaussian_process.kernels import RBF
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.naive_bayes import GaussianNB
@@ -22,29 +18,21 @@ from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 
 h = .02  # step size in the mesh
 
-names = ["Nearest Neighbors", "Linear SVM", "RBF SVM", "Gaussian Process",
-         "Decision Tree", "Random Forest", "Neural Net", "AdaBoost",
-         "Naive Bayes", "QDA"]
+names = ["Nearest Neighbors", "Decision Tree", "Random Forest", "Naive Bayes", "QDA"]
 
 classifiers = [
     KNeighborsClassifier(3),
-    SVC(kernel="linear", C=0.025),
-    SVC(gamma=2, C=1),
-    GaussianProcessClassifier(1.0 * RBF(1.0)),
     DecisionTreeClassifier(max_depth=5),
     RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
-    MLPClassifier(alpha=1, max_iter=1000),
-    AdaBoostClassifier(),
     GaussianNB(),
     QuadraticDiscriminantAnalysis()]
 
-X, y = make_classification(n_features=2, n_redundant=0, n_informative=2,
-                           random_state=1, n_clusters_per_class=1)
+X, y = make_classification(n_features=2, n_redundant=0, n_informative=2, random_state=1, n_clusters_per_class=1)
 rng = np.random.RandomState(2)
 X += 2 * rng.uniform(size=X.shape)
 linearly_separable = (X, y)
 
-ds = pd.read_csv("2011_fire_data.csv").to_numpy()
+ds = pd.read_csv("fire_data_2011.csv").to_numpy()
 
 figure = plt.figure(figsize=(27, 9))
 i = 1
@@ -76,6 +64,7 @@ i += 1
 
 # iterate over classifiers
 for name, clf in zip(names, classifiers):
+    print(name, ": Progress 1/3")
     ax = plt.subplot(1, len(classifiers) + 1, i)
     clf.fit(X_train, y_train.astype('int'))
     score = clf.score(X_test, y_test.astype('int'))
@@ -85,16 +74,16 @@ for name, clf in zip(names, classifiers):
     if hasattr(clf, "decision_function"):
         Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
     else:
-        print("decision func not found")
         Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
 
     # Put the result into a color plot
-    print(Z[0:].shape, i)
     if i == 3:
         Z = Z.reshape(xx.shape[0], xx.shape[1])
     else:
         Z = Z.reshape(xx.shape[0], xx.shape[1])
     ax.contourf(xx, yy, Z, cmap=cm, alpha=.8)
+
+    print(name, ": Progress 2/3")
 
     # Plot the training points
     ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright, edgecolors='k')
@@ -105,6 +94,8 @@ for name, clf in zip(names, classifiers):
     ax.set_ylim(yy.min(), yy.max())
     ax.set_xticks(())
     ax.set_yticks(())
+
+    print(name, ": Progress 3/3")
 
     ax.set_title(name)
     ax.text(xx.max() - .3, yy.min() + .3, ('%.2f' % score).lstrip('0'), size=15, horizontalalignment='right')
